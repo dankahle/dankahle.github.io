eaf9357cc0638390966e484284c069abdee1f21b
Dan Kahle <dan.kahle@gmail.com>
2015-08-09 08:10:38 -0700

    have sm/md nav working, but not remembering views yet

---
+++ app/app.js
@@ -0,0 +1,71 @@
+
+var log = console.log.bind(console);
+
+function safeDigest(scope) {
+	var dummy = scope.$$phase || (scope.$root && scope.$root.$$phase) || scope.$digest();
+}
+
+
+var app = angular.module('app', ['ngAnimate', 'ngTouch']);
+
+
+app.controller('bodyCtrl', function ($scope, $compile) {
+	window.$scope = $scope;
+
+
+
+
+	window.curMedia = '';
+	$(window).resize(function() {
+		var size;
+		if((size = getMedia()) != curMedia) {
+			log('change media', size)
+			curMedia = size;
+			//$scope.$apply('curMedia = "' + size + '"');
+			if(curMedia == 'md' && $scope.view == 'profile')
+				changeView('expr');
+
+		if(curMedia == 'xs' || curMedia == 'sm')
+			changeView('profile');
+		else if(curMedia == 'md') {
+			changeView('expr');
+		}
+		else if(curMedia == 'lg')
+			changeView('all');
+		}
+		safeDigest($scope);
+	})
+
+
+	var changeView = $scope.changeView = function(view) {
+		if($scope.view != view) {
+			$scope.view = view;
+			log('change view', view)
+			var $all = $('.profile, .expr, .github, .blog');
+
+			if(view == 'all') {
+				$all.hide().removeClass('in').show();
+				setTimeout(function() {
+					$all.addClass('in');
+				}, 100);
+			}
+
+			else {
+				if(curMedia != 'md') {
+					$('.profile').hide().removeClass('in');
+				}
+				$('.expr, .github, .blog').hide().removeClass('in');
+				var $view = $('.' + view);
+				$view.show();
+				setTimeout(function() {
+					$view.addClass('in');
+				}, 100)
+
+			}
+
+		}
+	}
+
+
+	$(window).resize();
+})
---
+++ js/site.js
@@ -0,0 +1,34 @@
+
+
+// globals
+window.log = console.log.bind(console);
+window.$body = $(document.body);
+window.media = {sm: 768, md: 992, lg: 1200};
+
+window.getMedia = function() {
+	var width = innerWidth;
+	if (width >= media.lg)
+		return 'lg';
+	if (width >= media.md)
+		return 'md';
+	if (width >= media.sm)
+		return 'sm';
+	else
+		return 'xs';
+
+}
+
+
+var $intro = $('.filter, .sm-md-nav, .profile, .expr, .github, .blog');
+
+$intro.addClass('intro')
+setTimeout(function() {
+	$intro.addClass('in');
+}, 1000)
+
+$('.filter').on('transitionend webkitTransitionEnd', function() {
+	$('.filter').off('transitionend webkitTransitionEnd');
+	$intro.removeClass('intro');
+})
+
+
