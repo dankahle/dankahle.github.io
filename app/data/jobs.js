c4c27a8c55470db84bb7b988ee3ac9844099916a
Dan Kahle <dan.kahle@gmail.com>
2015-08-09 18:40:57 -0700

    have expr opacity working

--- app/data/jobs.js
+++ app/data/jobs.js
@@ -3,7 +3,7 @@ var jobs = [
 	{
 		title: 'title one',
 		desc: 'desc one',
-		summary: 'summary one'
+		summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.'
 	},
 	{
 		title: 'title two',
--- app/directives.js
+++ app/directives.js
@@ -5,14 +5,10 @@ app.directive('dkAccordion', function () {
    return {
       scope: true,
       link: function ($scope, elem, attr) {
-         log('accord', $scope)
-         window.$scopeAccord = $scope;
 
          $scope.curSummary = -1;
-
          $scope.toggleSummary = function (index) {
             $scope.curSummary = $scope.curSummary === index ? -1 : index;
-            log($scope.curSummary)
          }
 
       }
@@ -29,8 +25,6 @@ app.directive('dkExpr', function () {
                   '<div class="summary" ng-show="curSummary == $index">{{job.summary}}</div> ' +
                '</div>',
       link: function ($scope, elem, attr) {
-         log('child', $scope)
-         window.$scopeChild = $scope;
 
       }
    }
