971ed31d406a52b64517d16320adbd7bffbfc1fb
Dan Kahle <dan.kahle@gmail.com>
2015-08-12 14:06:20 -0700

    fixed intro and media changes

--- app/app.js
+++ app/app.js
@@ -1,8 +1,7 @@
-
 var log = console.log.bind(console);
 
 function safeDigest(scope) {
-	var dummy = scope.$$phase || (scope.$root && scope.$root.$$phase) || scope.$digest();
+   var dummy = scope.$$phase || (scope.$root && scope.$root.$$phase) || scope.$digest();
 }
 
 
@@ -10,77 +9,125 @@ var app = angular.module('app', ['ngAnimate', 'ngTouch', 'ngSanitize', 'dkModal'
 
 
 app.controller('bodyCtrl', function ($scope, $compile) {
-	window.$scope = $scope;
+   window.$scope = $scope;
 
-	$scope.arr = [1,2,3];
+   $scope.arr = [1, 2, 3];
    $scope.skills = skills;
    $scope.jobs = jobs;
    $scope.repos = repos;
    $scope.blogs = blogs;
 
-	window.curMedia = '';
-	$(window).resize(function() {
-		var size;
-		if((size = getMedia()) != curMedia) {
-			//log('change media', size)
-			curMedia = size;
-			//$scope.$apply('curMedia = "' + size + '"');
-			if(curMedia == 'md' && $scope.view == 'profile')
-				changeView('expr', true);
+   var lastMedia, curMedia;
 
-		if(curMedia == 'xs' || curMedia == 'sm')
-			changeView('profile', true);
-		else if(curMedia == 'md') {
-			changeView('expr', true);
-		}
-		else if(curMedia == 'lg')
-			changeView('all', true);
-		}
-		safeDigest($scope);
-	})
+   $(window).resize(function () {
+      var media, $views = [];
+      if ((media = getMedia()) != curMedia) {
+         log('change media', media)
+         lastMedia = curMedia;
+         curMedia = media;
+         if (!lastMedia) { // initial entry
+            $views.push($nav);
 
-	var $phoneNav = $('.phone-nav');
+            if (_.contains(['xs', 'sm'], curMedia)) {
+               $scope.view = 'profile';
+               $views.push($profile);
+               $([$expr[0], $github[0], $blog[0]]).hide().removeClass('in');// leave profile showing
+            }
+            else if(curMedia == 'md') {
+               $scope.view = 'expr';
+               $([$github[0], $blog[0]]).hide().removeClass('in');
+               $views.push($profile);
+               $views.push($expr);
+            }
+            else if(curMedia == 'lg') {
+               setTimeout(function() {
+                  $([$profile[0], $expr[0], $github[0], $blog[0]]).addClass('in');
+               }, 100)
+            }
+         }
+         else {
+            if (_.contains(['xs', 'sm'], lastMedia) && curMedia == 'md') {
+               if ($scope.view == 'profile') {
+                  $scope.view = 'expr';
+                  $views.push($expr);
+               }
+               else {
+                  $views.push($profile);
+               }
+            }
+            else if (lastMedia == 'md' && curMedia == 'lg') {
+               [$expr, $github, $blog].forEach((function (v) {
+                  if (!v.hasClass('in'))
+                     $views.push(v);
+               }))
+            }
+            else if (lastMedia == 'lg' && curMedia == 'md') {
+               if (!$scope.view) {
+                  $scope.view = 'expr';
+                  $views.push($expr);
+                  $([$github[0], $blog[0]]).hide().removeClass('in');
+               }
+               else {
+                  // can't be profile cause have a view and went from md to lg with it
+                  // must be expr/github/blog, need to keep which one and hide the other two
+                  [$expr, $github, $blog].forEach(function(v) {
+                     if(v.selector != '.' + $scope.view)
+                        v.hide().removeClass('in');
+                  })
+               }
+            }
+            else if (lastMedia == 'md' && _.contains(['xs', 'sm'], curMedia)) {
+               if($scope.view == 'profile') {
+                  // do nothing
+               }
+               else {
+                  $profile.hide().removeClass('in');
+               }
 
-   var $profile = $('.profile');
-	var changeView = $scope.changeView = function(view, mediaChange) {
+            }
 
-		if(mediaChange || $scope.view != view) {
-			$scope.view = view;
-			//log('change view', view)
-			var $all = $('.profile, .expr, .github, .blog');
+         }
 
-			if(view == 'all') {
-				$all.hide().removeClass('in').show();
-				setTimeout(function() {
-					$all.addClass('in');
-				}, 100);
-			}
+         if ($views.length) {
+            console.log($views)
+            $views.forEach(function (v) {
+               if(v != $nav)// can't have element display style on nav
+                  v.show();
+            })
+            setTimeout(function () {
+               $views.forEach(function (v) {
+                  v.addClass('in');
+               })
+               safeDigest($scope);
+            }, 100)
+         }
+      }
+   })
 
-			else {
-				if(curMedia != 'md') {
-					$profile.hide().removeClass('in');
-				}
-				$('.expr, .github, .blog').hide().removeClass('in');
-				var $view = $('.' + view);
-				$view.show();
+   // change view for xs/sm(same) and md
+   $scope.changeView = function (view) {
 
-            if(curMedia == 'md' && mediaChange)
-               $profile.show();
+      if ($scope.view != view) {
+         $scope.view = view;
+         //log('change view', view)
 
-				setTimeout(function() {
-					$view.addClass('in');
+         if (curMedia != 'md') {
+            $profile.hide().removeClass('in');
+         }
+         $([$expr[0], $github[0], $blog[0]]).hide().removeClass('in');
+         var $view = $('.' + view);
+         $view.show();
 
-               if(curMedia == 'md' && mediaChange)
-                  $profile.addClass('in');
-				}, 100)
+         setTimeout(function () {
+            $view.addClass('in');
 
-			}
+            if (curMedia == 'md' && mediaChange)
+               $profile.addClass('in');
+         }, 100)
+      }
+   }
 
-		}
-	}
 
-
-
-	$(window).resize();
+   $(window).resize();
 })
 
